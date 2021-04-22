ff4ec92f00e153efe265723d03505375bb0f8eba
kutaykoylan <kerimkutay@gmail.com>
2021-04-23 00:50:17 +0300

    refactor: kafka port is taken from global

--- order-service/src/main/java/com/example/orderservice/config/KafkaConfig.java
+++ order-service/src/main/java/com/example/orderservice/config/KafkaConfig.java
@@ -2,6 +2,7 @@ package com.example.orderservice.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -15,13 +16,15 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
+    @Value(value = "${kafkaglobal.port}")
+    private String kafkaPort;
     @Bean
     public ProducerFactory<String, Object> producerFactory()
     {
+        System.out.println(kafkaPort);
         Map<String,Object> config = new HashMap<>();
 
-//      Burdaki portu falan env variable dan alalim da nasi aliyoz javada bakmak lazim
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:"+kafkaPort);
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
